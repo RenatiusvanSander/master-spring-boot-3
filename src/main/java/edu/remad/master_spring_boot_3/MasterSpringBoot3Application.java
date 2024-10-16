6ea72a9d1573d73a1011e5ba2cf7e2686c531e7c
RenatiusvanSander <remad@web.de>
2024-10-16 18:00:10 +0200

    master: initial project setup

---
+++ src/main/java/edu/remad/master_spring_boot_3/MasterSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package edu.remad.master_spring_boot_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSpringBoot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/remad/master_spring_boot_3/MasterSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.remad.master_spring_boot_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
